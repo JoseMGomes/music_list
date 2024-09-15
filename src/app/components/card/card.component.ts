027fa873eb2486ebb64d2401e020d2364fd68c27
José Lucas <gomessze03@gmail.com>
2024-09-15 15:36:11 -0300

    close #3:cards were created for selecting playlists

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  sertanejoColor: string = 'bg-secondary';
+  funkColor: string = 'bg-secondary';
+  pagodeColor: string = 'bg-secondary';
+  forroColor: string = 'bg-secondary';
 
+  resetColors() {
+    this.sertanejoColor = 'bg-secondary';
+    this.funkColor = 'bg-secondary';
+    this.pagodeColor = 'bg-secondary';
+    this.forroColor = 'bg-secondary';
+  }
+
+  chargeSertanejo() {
+    this.resetColors();
+    this.sertanejoColor = 'bg-primary text-white';
+  }
+  chargeFunk() {
+    this.resetColors();
+    this.funkColor = 'bg-primary text-white';
+  }
+  chargePagode() {
+    this.resetColors();
+    this.pagodeColor = 'bg-primary text-white';
+  }
+  chargeForro() {
+    this.resetColors();
+    this.forroColor = 'bg-primary text-white';
+  }
 }
